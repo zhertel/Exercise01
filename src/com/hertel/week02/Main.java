4f9b8ccf2c8a43a741984c682f931243e9a6d91a
zhertel <33462459+zhertel@users.noreply.github.com>
2018-01-23 13:09:56 -0500

    Week 2

---
+++ src/com/hertel/week02/Main.java
@@ -0,0 +1,11 @@
+package com.hertel.week02;
+/*
+Zach Hertel
+1/23/18
+Week 2 Examples
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to week 2!");
+    }
+}
